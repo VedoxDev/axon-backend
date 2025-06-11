2c368b56e9379e6d0e74c13cf5a5f5e6e2f4faf8
VedoxDev <v.a.fonseca.y@gmail.com>
2025-06-11 12:30:37 +0200

    chore(auth) Autenticacion configuracion

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -14,7 +14,7 @@ import { EmailService } from 'src/common/services/email.service';
     TypeOrmModule.forFeature([User]), // Add User repository for JwtStrategy
     JwtModule.register({
       secret: process.env.JWT_SECRET, 
-      signOptions: { expiresIn: '1d'}
+      signOptions: { expiresIn: '14d'}
     })
   ],
   controllers: [AuthController],
